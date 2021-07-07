78deefbd39ec372a43dd085fdf2da1c472800ae4
saico09 <marc.contrerasd@duocuc.cl>
2021-07-07 18:42:50 -0400

    ultima clase

--- README.md
+++
@@ -1,7 +0,0 @@
-# proyectosemestralweb
-programacion WEB
-
-Marco Antonio Contreras Díaz (Líder)
-Javier André Molina Chávez
-
-Caso 2:Caos News - noticias
--- VERSION DJANGO/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Contacto,Registrar,Publicar
-
-# Register your models here.
-
-admin.site.register(Contacto)
-admin.site.register(Registrar)
-admin.site.register(Publicar)
-admin.site.register(Categoria)
